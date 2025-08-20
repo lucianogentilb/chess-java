62285663cd665fa3c37c94d6a77e31d69d5031b7
Batistalb <lb.gentil@gmail.com>
2025-08-20 11:07:11 -0400

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,10 +56,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK !!!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK !");
+			}	
+		}
+		else {
+			System.out.println("!!!  C H E C K M A T E  !!!");
+			System.out.println("Winner: ****** " + chessMatch.getCurrentPlayer() + " ******");
 		}
 	}
 
